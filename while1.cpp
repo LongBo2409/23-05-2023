bb906fdf58ec38588e89d5757bf45163aa6a954a
LongBo2409 <97206434+LongBo2409@users.noreply.github.com>
2023-05-23 09:07:04 +0700

    Add files via upload

---
+++ while1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main (){
+//	int num = 0;
+//	while (num <= 5){
+//		printf("\n%d", num);
+//		num++;
+//	}
+//	return 0;
+
+	int num = 0;
+	do {
+		printf("\n%d", num);
+		num++;
+	}while (num <= 5);
+	return 0;
+}
